e33f592b9812920104f1bb870b10b676c5e3abfc
DannyArends <Danny.Arends@gmail.com>
2009-01-22 15:09:28 +0100

    =Update of the testfiles in MQMpackage/TestData. Also added V_0.exe which is an unedited compiled version of the MQM algorithm. Changed The prepareMQM, now this includes a function to test the output of any new executables versus the the V_0.exe version (SOME OS dependant code involved) which will be removed when the MQM algorithm is implemented as a R/DLL file

--- MQMpackage/R/prepareMQM.R
+++ MQMpackage/R/prepareMQM.R
@@ -12,6 +12,9 @@
 ######################################################################
 
 prepareMQM <- function(cross, name){
+#print files needed by the MQM algorithm (Genotype & Phenotype, also calls 
+#the construction function of the run-input file)
+
  f1mar <- NULL
  f2mar <- t(pull.geno(cross))
  for(i in 1:dim(f2mar)[1]) {
@@ -29,22 +32,19 @@ prepareMQM <- function(cross, name){
    }
  }
  rownames(f1mar) = rownames(f2mar)
- print("Going to write files\n")
  filename <- paste(name,"_F2.MAR.TXT", sep="")
  write.table(f2mar, file = filename, col.names=FALSE, quote=FALSE)
- print("f2 markers writen\n")
- 
+
  filename <- paste(name,"_F2.QUA.TXT",sep="")
  f2qua <- pull.pheno(cross)
  write.table(t(f2qua), file = filename, row.names=FALSE,col.names=FALSE, quote=FALSE)
- print("f2 genotypes writen\n")
- MQM_in <- printMQMin(cross, name)
- 
+ MQM_in <- printMQMin(cross,name) 
 }
 
 
-printMQMin <- function(cross, name){
-	# Printing header
+printMQMin <- function(cross,name){
+#print mqm_in.txt file needed by the MQM algorithm
+	
 	n.individuals <- nind(cross)
 	n.fam <- 1
 	n.mark <- sum(nmar(cross))
@@ -53,7 +53,7 @@ printMQMin <- function(cross, name){
 	info <- rbind(info,c("Nfam=",n.fam))
 	info <- rbind(info,c("Nmark=",n.mark))
 	
-	write.table(info, file = paste(name,"_mqm_in.txt",sep=""), append = FALSE, sep = " ",col.names=FALSE,row.names=FALSE, quote=FALSE)
+	write.table(info, file = "mqm_in.txt", append = FALSE, sep = " ",col.names=FALSE,row.names=FALSE, quote=FALSE)
 	
 	# Printing the markers per chromosome
 	n.chr <- nchr(cross)
@@ -66,7 +66,7 @@ printMQMin <- function(cross, name){
 		}
 	}
 	result
-	write.table(result, file = paste(name,"_mqm_in.txt",sep=""), append = TRUE, sep = " ",col.names=FALSE,row.names=FALSE, quote=FALSE)
+	write.table(result, file = "mqm_in.txt", append = TRUE, sep = " ",col.names=FALSE,row.names=FALSE, quote=FALSE)
 	
 	settings <- NULL;
 	settings <- rbind(settings,c("Cross=","f2"))
@@ -78,29 +78,70 @@ printMQMin <- function(cross, name){
 	settings <- rbind(settings,c("defset=","y"))
 	settings <- rbind(settings,c("real_simu=","0"))
 	settings <- rbind(settings,c("perm_simu=","1 0"))
-	write.table(as.matrix(settings), file = paste(name,"_mqm_in.txt",sep=""), append = TRUE, sep = " ",col.names=FALSE,row.names=FALSE, quote=FALSE)	
+	write.table(as.matrix(settings), file = "mqm_in.txt", append = TRUE, sep = " ",col.names=FALSE,row.names=FALSE, quote=FALSE)	
 }
 
-GenerateTestSets <- function(){
+RunTest <- function(testset = "T1", exe="V_1.exe"){
+#Runs an executable versus a, and compares the output to the output from runPrelimTest 
+ 
+ shell("del mqm_out.txt", intern=TRUE) 
+ #read old output
+ v0.output <- read.table(paste("V_0/",testset,"_OUT",sep=""))
+ #setup the In-File for testing
+ outcome <- shell(paste("copy V_0\\",testset,"_mqm_in.txt mqm_in.txt",sep=""), intern=TRUE)
+ outcome <- shell(exe, intern=TRUE)
+ vNew_out <- readMQMout()
+ error <- 0
+ for(i in 1:dim(v0.output)[1]) {
+   for(j in 1:dim(v0.output)[2]) {
+    if(abs(v0.output[i,j] - vNew_out[i,j]) > 0.01){
+	#print(paste("WARNING No match between old and new version at: ",i,j,"",sep=""))
+	error <- 1;
+	}
+  }
+ }
+ if(error == 0){
+   print(paste("No error detected between V_0.exe and ",exe,sep=""))
+ }else{
+   print(paste("Errors detected between V_0.exe and ",exe,sep=""))
+ }
+}
+
+RunPrelimTest <- function(testset = "T1",qtl = NULL){
+#Runs MQM (original version on a testset)
+#Places files in an "V_0" directory (which should be setup in advance by the user
+
    library(qtl)
+   setwd("D:/MQM/compiled")
+   library(qtl)
+   shell("del mqm_out.txt", intern=TRUE)    # make sure the output from the previous runs are gone
    data(map10)
-   cross <- sim.cross(map10,n=100)
-   prepareMQM(cross,"T1")
+   cross <- sim.cross(map10,qtl,n=100)
+   prepareMQM(cross,testset)
+   outcome <- shell("V_0.exe", intern=TRUE)
+   write.table(readMQMout(), file = paste("V_0/",testset,"_OUT",sep=""), row.names=FALSE,col.names=FALSE, quote=FALSE)
+   shell(paste("copy mqm_in.txt V_0\\",testset,"_mqm_in.txt",sep=""), intern=TRUE)   #Stores the runFILE & output to a V_0 directory
+   shell("del mqm_out.txt", intern=TRUE)   											#make sure
+   shell("del mqm_in.txt", intern=TRUE)    											# also replace the input file (for the new run
+}
 
-   data(map10)
-   cross <- sim.cross(map10,c(1,30,1,0),n=100)
-   prepareMQM(cross,"T2")
 
-   data(map10)
-   cross <- sim.cross(map10,c(8,-12,1,2),n=100)
-   prepareMQM(cross,"T3")
+GenerateTestSets <- function(){
+#Run once version to setup some testfiles
+#Executes "V_0.exe" to get output for each testfile (so we can compare newer versions)
 
-   data(map10)
-   cross <- sim.cross(map10,rbind(c(1,2,1,2),c(4,23,-1,2)),n=100)
-   prepareMQM(cross,"T4") 
+   RunPrelimTest(testset="T1")
+   RunPrelimTest(testset="T2",qtl=c(1,30,1,0))
+   RunPrelimTest(testset="T3",qtl=c(19,30,0,1))
+   RunPrelimTest(testset="T4",qtl=c(19,500,0,1))
+   RunPrelimTest(testset="T5",qtl=rbind(c(19,5,0,1),c(19,45,0,1)))
+   RunPrelimTest(testset="T6",qtl=rbind(c(19,5,0,1),c(19,45,0,-1)))
+
 }
 
 readMQMout <- function(cross = NULL, file = "mqm_out.txt", plot = FALSE){
+#reads the output from the MQM algorithm
+
    data <-read.table(file, quote=":")
    data <-data[-dim(data)[1],]
    if(plot){
